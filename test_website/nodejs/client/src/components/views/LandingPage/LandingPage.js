893e772d091b178d3ab64277f270344ffccf34e6
Jotter-Vortex <ckdwls525@gmail.com>
2022-04-27 20:27:49 +0900

    DB에서 가져온 모든 정보 정리 후 웹 페이지에 뿌리기
    DB에서 가져온 모든 정보 정리 후 웹 페이지에 뿌리기

--- test_website/nodejs/client/src/components/views/LandingPage/LandingPage.js
+++ test_website/nodejs/client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from "axios";
+import {Icon, Col, Cards, Row, Card, Carousel} from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import Column from 'antd/lib/table/Column';
+import ImageSlider from '../../utils/ImageSlider';
 
 function LandingPage() {
+
+    const [Products, setProducts] = useState([])
 
     useEffect(()=> {
         axios.post('/api/product/products')
         .then(response => {
             if (response.data.success){
                 console.log(response.data)
+                setProducts(response.data.productInfo)
             }
             else{
                 alert("상품들을 가져오는데 실패했습니다.")
@@ -18,9 +25,48 @@ function LandingPage() {
 
      }, [])
 
+     const renderCards = Products.map((product, index) => {
+
+        console.log('product', product)
+
+        return <Col lg={6} md={8} xs={24} key = {index}>
+        
+        <Card
+            cover= {<ImageSlider images = {product.images}></ImageSlider>}>
+            
+            <Meta
+                title = {product.title}
+                description = {`$${product.price}`}></Meta>
+        </Card>
+
+        </Col>
+     })
+
 
     return (
-        <div>landing page</div>
+        <div style = {{width:'75%', margin: '3rem auto'}}>
+
+            <div style= {{textAlign : 'center'}}>
+                <h2>Let's Travel Anyware!!!<Icon type = "rocket"></Icon></h2>
+            </div>
+            
+            {/* Filter */}
+
+            {/* Search */}
+
+            {/* Cards */}
+
+
+            <Row gutter= {[16,16]}>
+                {renderCards}
+            </Row>
+            
+
+            <div style={{justifyContent:'center'}}>
+                <button>더보기</button>
+            </div>
+
+        </div>
     )
 }
 
