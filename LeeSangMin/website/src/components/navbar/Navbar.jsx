7490e095ea9f03791ae47266a8110facb47152e6
SM922 <ddi03090@naver.com>
2022-04-30 12:47:42 +0900

    portal proto

--- LeeSangMin/website/src/components/navbar/Navbar.jsx
+++ LeeSangMin/website/src/components/navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./Navbar.scss"
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import LanguageIcon from '@mui/icons-material/Language';
 
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          {/* <input type="text" placeholder="search.." /> */}
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon className="icon"/>
+          </div>
+          <div className="item">
+            <NightlightIcon className="icon"/>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
--- LeeSangMin/website/src/components/sidebar/Sidebar.jsx
+++ LeeSangMin/website/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import "./Sidebar.scss"
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">Sidebar</div>
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">DASH BOARD</span>
+      </div>
+      <hr/>
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon"/>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <ContentPasteSearchIcon className="icon"/>
+            <span>Details</span>
+          </li>
+          <li>
+            <BarChartIcon className="icon"/>
+            <span>Charts</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <NotificationsNoneIcon className="icon"/>
+            <span>Alarm</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon"/>
+            <span>Settings</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    
+    </div>
   )
 }
 
