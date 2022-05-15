114232bcaa3a175d2221205f3601479f42fca922
SM922 <ddi03090@naver.com>
2022-05-15 22:50:33 +0900

    Monitoring Web Rollback
    - Rollback all files

--- monitoring_web/src/components/report_table/Report_table.jsx
+++ monitoring_web/src/components/report_table/Report_table.jsx
@@ -1,149 +1,410 @@
 import React from 'react'
-import "./Report_table.scss"
+import PropTypes from 'prop-types';
+import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Checkbox from '@mui/material/Checkbox';
+// import IconButton from '@mui/material/IconButton';
+// import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+// import DeleteIcon from '@mui/icons-material/Delete';
+// import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+import {Link} from "react-router-dom"
 
-const Report_table = () => {
+
+  //표시할 테이블 데이터 db값 가져오기
+  //##주의! DB에서 timestamp 중복값으로 가져오면 안됨. -> 쿼리문 사용해서 같은 timestamp를 가진 데이터를 한 raw에 계산하여 삽입해야함
+  const rows = [
+    createData('2022-04-16T09:27:18Z',"65.61.137.117", "testwebsiteteam2.shop", 9,1),
+    createData('2022-04-15T10:27:18Z',"65.61.137.117", "testwebsiteteam2.shop", 6,0),
+    createData('2022-05-14T15:27:18Z',"65.61.137.117", "testwebsiteteam2.shop", 7,1),
+    createData('2022-05-13T11:27:18Z',"65.61.137.117", "testwebsiteteam2.shop", 5,0),
+    createData('2022-04-12T08:27:18Z',"65.61.137.117", "testwebsiteteam2.shop", 1,2),
+    createData('2022-04-11T07:27:18Z',"65.61.137.117", "testwebsiteteam2.shop", 10,1),
+    createData('2022-04-10T20:27:18Z',"65.61.137.117", "testwebsiteteam2.shop", 8,1),
+    createData('2022-04-09T14:27:18Z',"65.61.137.117", "testwebsiteteam2.shop", 7,0),
+    createData('2022-04-08T18:27:18Z',"65.61.137.117", "testwebsiteteam2.shop", 5,0),
+  ];
+
+
+
+const REPORT_TABLE = () => {
+
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('Timestamp');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.Timestamp);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+
   return (
     <div className = "report_table">
-        <TableContainer component={Paper}>
-      <Table aria-label="collapsible table">
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>Diagnosis Date</TableCell>
-            <TableCell align="center">Reported Vulnerabilities</TableCell>
-            <TableCell align="center">IP</TableCell>
-            <TableCell align="center">Solved</TableCell>
-            <TableCell align="center">Notice</TableCell>
-            
-            {/* <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <Row key={row.name} row={row} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+       <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2}}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+          >
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}handleClick
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.Timestamp);//
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.Timestamp)}//
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.Timestamp}//
+                      selected={isItemSelected}
+                      
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            'aria-labelledby': labelId,
+                          }}
+                        />
+                      </TableCell>
+                      
+                      
+                        <TableCell
+                          component="th"
+                          id={labelId}
+                          scope="row"
+                          padding="none"
+                          align = "center"
+                          sx={{ fontSize:17}}
+                        >
+                          <Link to="/details" style={{textDecoration:"none"}}>
+                          {row.Timestamp}                         
+                          </Link>
+                        </TableCell>
+                      
+                      <TableCell 
+                        align="center"
+                        sx={{ fontSize:17}}
+                        >{row.Hostname}</TableCell>
+                      <TableCell 
+                        align="center"
+                        sx={{ fontSize:17}}
+                        >{row.IP}</TableCell>
+                      <TableCell 
+                        align="center"
+                        sx={{ fontSize:17}}
+                        >{row.NumberOfFound}</TableCell>
+                      <TableCell 
+                        align="center"
+                        sx={{ fontSize:17}}
+                        >{row.Warning}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (dense ? 33 : 53) * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 20]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Shrinking"
+      />
+    </Box>
+
+         
     </div>
   )
 }
 
-function createData(
-    date: string,
-    Vulnerabilities: number,
-    IP: string,
-    Solved: number,
-    Notice: number,
-  ) {
-    return {
-      date,
-      Vulnerabilities,
-      IP,
-      Solved,
-      Notice,
-      history: [
-        {
-          date: 'Tue Apr 26 06:30:00 2022 UTC',
-          NVT: 'Cleartext Transmission of Sensitive Information via HTTP',
-          Threat: 'Medium',
-          Details: "The host application transmits sensitive information (username, passwords) in cleartext via HTTP.",
-        },
-        {
-          date: 'Thu Apr 27 06:30:00 2022 UTC',
-          NVT: 'Cleartext Transmission of Sensitive Information via HTTP',
-          Threat: 'Medium',
-          Details: "The host application transmits sensitive information (username, passwords) in cleartext via HTTP.",
-        },
-      ],
-    };
+
+function createData(Timestamp, IP, Hostname, NumberOfFound, Warning) {
+  return {
+    Timestamp,
+    IP,
+    Hostname,
+    NumberOfFound,
+    Warning,
+  };
+}
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
   }
-  
-  function Row(props: { row: ReturnType<typeof createData> }) {
-    const { row } = props;
-    const [open, setOpen] = React.useState(false);
-  
-    return (
-      <React.Fragment>
-        <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-          <TableCell>
-            <IconButton
-              aria-label="expand row"
-              size="small"
-              onClick={() => setOpen(!open)}
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: 'Timestamp', 
+    numeric: false,
+    disablePadding: true,
+    label: 'Diagnosis Date',
+  },
+  {
+    id: 'Hostname',
+    numeric: false,
+    disablePadding: false,
+    label: 'Hostname',
+  },
+  {
+    id: 'IP',
+    numeric: false,
+    disablePadding: false,
+    label: 'IP',
+  },
+  {
+    id: 'NumberOfFound',
+    numeric: true,
+    disablePadding: false,
+    label: 'Number of vulnerabilities found',
+  },
+  {
+    id: 'Warning',
+    numeric: true,
+    disablePadding: false,
+    label: 'Warning',
+  },
+];
+
+function EnhancedTableHead(props) {
+  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+    props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox" >
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              'aria-label': 'select all desserts',
+            }}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (    
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'center' : 'center'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+            sx={{ fontSize:20, fontWeight:700 }}
+          > 
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
             >
-              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
           </TableCell>
-          <TableCell component="th" scope="row">
-            {row.date}
-          </TableCell>
-          <TableCell align="center">{row.Vulnerabilities}</TableCell>
-          <TableCell align="center">{row.IP}</TableCell>
-          <TableCell align="center">{row.Solved}</TableCell>
-          <TableCell align="center">{row.Notice}</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <Box sx={{ margin: 1 }}>
-                <Typography variant="h6" gutterBottom component="div">
-                  Report
-                </Typography>
-                <Table size="small" aria-label="purchases">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>NVT</TableCell>
-                      <TableCell>Details</TableCell>
-                      <TableCell align="left">Severity</TableCell>
-                      {/* <TableCell align="right">port</TableCell> */}
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {row.history.map((historyRow) => (
-                      <TableRow key={historyRow.date}>
-                        <TableCell component="th" scope="row">
-                          {historyRow.NVT}
-                        </TableCell>
-                        <TableCell>{historyRow.Details}</TableCell>
-                        <TableCell align="left">{historyRow.Threat}</TableCell>
-                        {/* <TableCell align="right">
-                          {Math.round(historyRow.amount * row.price * 100) / 100}
-                        </TableCell> */}
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </Box>
-            </Collapse>
-          </TableCell>
-        </TableRow>
-      </React.Fragment>
-    );
-  }
-  
-  const rows = [
-    createData('Tue Apr 26 06:30:00 2022 UTC', 12, "65.61.137.117", 10, 2, 1),
-    createData('Wed Apr 27 06:30:00 2022 UTC', 23, "65.61.137.117", 22, 1, 0),
-    createData('Thu Apr 28 06:30:00 2022 UTC', 26, "65.61.137.117", 26, 0, 0),
-    createData('Fri Apr 29 06:30:00 2022 UTC', 30, "65.61.137.117", 30, 0, 1),
-    createData('Sat Apr 30 06:30:00 2022 UTC', 35, "65.61.137.117", 34, 1, 2),
-  ];
+
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const EnhancedTableToolbar = (props) => {
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: '1 1 100%' }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: '1 1 100%', fontSize : 25}}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          Diagnosis History
+        </Typography>
+      )}
+
+      {/* {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <DeleteIcon /> 
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton>
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )} */}
+    </Toolbar>
+  );
+};
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
 
   
 
-export default Report_table
+
+  
+
+export default REPORT_TABLE
--- monitoring_web/src/pages/home/Home.jsx
+++ monitoring_web/src/pages/home/Home.jsx
@@ -7,16 +7,13 @@ import Widget2 from "../../components/widget/Widget2";
 import Widget3 from "../../components/widget/Widget3";
 import Chart1 from "../../components/chart/Chart1";
 import Chart2 from "../../components/chart/Chart2";
-import Report_table from "../../components/report_table/Report_table";
+import REPORT_TABLE from "../../components/report_table/Report_table";
 
-import Box from '@mui/material/Box';
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
+
+// import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+// import SaveIcon from '@mui/icons-material/Save';
+// import PrintIcon from '@mui/icons-material/Print';
+// import ShareIcon from '@mui/icons-material/Share';
 
 /** 메인 대쉬보드 **/
 
@@ -43,7 +40,7 @@ const Home = () => {
           <div className="ReportTableContainer">
             <div className="ReportTable">
               {/* 리포트 테이블 */}
-              <Report_table/>
+              <REPORT_TABLE/>
             </div>
             </div>  
         </div>
@@ -51,11 +48,11 @@ const Home = () => {
   )
 }
 
-const actions = [
-  { icon: <FileCopyIcon />, name: 'Copy' },
-  { icon: <SaveIcon />, name: 'Save' },
-  { icon: <PrintIcon />, name: 'Print' },
-  { icon: <ShareIcon />, name: 'Share' },
-];
+// const actions = [
+//   { icon: <FileCopyIcon />, name: 'Copy' },
+//   { icon: <SaveIcon />, name: 'Save' },
+//   { icon: <PrintIcon />, name: 'Print' },
+//   { icon: <ShareIcon />, name: 'Share' },
+// ];
 
 export default Home;
