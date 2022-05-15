50acbe8f577e5ceec5875cb86eef31c39382304a
changjinlee <54494793+Jotter-Vortex@users.noreply.github.com>
2022-05-15 22:38:58 +0900

    Revert "Update App.js"
    This reverts commit 958bd2ef17482ab43981e79de454bec279c282d9.

--- monitoring_web/src/App.js
+++ monitoring_web/src/App.js
@@ -2,8 +2,7 @@ import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import Details from "./pages/details/Details"
 import Chart from "./pages/charts/Chart"
-import React, { useState } from "react"
-import { DbProvider } from "./db/dbContext"
+import Reportpage from "./pages/reportpage/Reportpage"
 
 import {
   BrowserRouter,
@@ -15,20 +14,19 @@ import {
 function App() {
   return (
     <div className="App">
-      <DbProvider>
-        <BrowserRouter>
-          {/* React-Router-Dom -> 페이지간 링크 구축*/}
-          <Routes>
-            <Route path="/">
-              <Route index element={<Home />}></Route>
-              <Route path="login" element={<Login />}></Route>
-              <Route path="details" element={<Details />}></Route>
-              <Route path="chart" element={<Chart />}></Route>
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </DbProvider>
-    </div>
+      <BrowserRouter>
+    {/* React-Router-Dom -> 페이지간 링크 구축*/}
+     <Routes>
+       <Route path="/">
+         <Route index element = {<Home/>}></Route>
+         <Route path="login" element={<Login/>}></Route>
+         <Route path="details" element={<Details/>}></Route>
+         <Route path="chart" element={<Chart/>}></Route>
+         <Route path="details/reportpage" element={<Reportpage/>}></Route>
+       </Route>
+     </Routes>
+    </BrowserRouter>
+  </div>
   );
 }
 
