958bd2ef17482ab43981e79de454bec279c282d9
박건우 <33975284+qkrrjsdn@users.noreply.github.com>
2022-05-15 21:42:26 +0900

    Update App.js

--- monitoring_web/src/App.js
+++ monitoring_web/src/App.js
@@ -2,7 +2,8 @@ import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import Details from "./pages/details/Details"
 import Chart from "./pages/charts/Chart"
-import Reportpage from "./pages/reportpage/Reportpage"
+import React, { useState } from "react"
+import { DbProvider } from "./db/dbContext"
 
 import {
   BrowserRouter,
@@ -14,19 +15,20 @@ import {
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-    {/* React-Router-Dom -> 페이지간 링크 구축*/}
-     <Routes>
-       <Route path="/">
-         <Route index element = {<Home/>}></Route>
-         <Route path="login" element={<Login/>}></Route>
-         <Route path="details" element={<Details/>}></Route>
-         <Route path="chart" element={<Chart/>}></Route>
-         <Route path="details/reportpage" element={<Reportpage/>}></Route>
-       </Route>
-     </Routes>
-    </BrowserRouter>
-  </div>
+      <DbProvider>
+        <BrowserRouter>
+          {/* React-Router-Dom -> 페이지간 링크 구축*/}
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home />}></Route>
+              <Route path="login" element={<Login />}></Route>
+              <Route path="details" element={<Details />}></Route>
+              <Route path="chart" element={<Chart />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </DbProvider>
+    </div>
   );
 }
 
