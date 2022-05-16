a01a539176d40842a1fbf4def794d61ed283e253
changjinlee <54494793+Jotter-Vortex@users.noreply.github.com>
2022-05-16 09:51:16 +0900

    Flask 테스트 웹 서버 이메일 기능 누락 내용
    깃허브 사용중 충돌로 인해 생긴 파일 누락 부분 다시 커밋

--- test_website/Flask/app.py
+++ test_website/Flask/app.py
@@ -1,6 +1,7 @@
 from bson import ObjectId
 from flask import Flask, render_template, jsonify, request, sessions
 from pymongo import MongoClient
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 MONGODB_ID = "ID"
@@ -14,6 +15,18 @@ db = client.knutesting
 @app.route('/')
 def home():
    return render_template('index.html')
+   
+@app.route('/knu_detail')
+def knu_detail():
+   return render_template('knu_detail.html')
+
+@app.route('/knu_post')
+def knu_post():
+   return render_template('knu_post.html')
+
+@app.route('/knu_contact')
+def knu_contact():
+   return render_template('knu_contact.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
