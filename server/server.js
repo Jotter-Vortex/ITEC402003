c2243c14b85f956d73af00600a6f355117b0f067
박건우 <33975284+qkrrjsdn@users.noreply.github.com>
2022-05-18 01:56:56 +0900

    db의 이름 변경 및 모든 데이터 한번에 보내게 바꿨습니다
    db의 collection이름은 timestamp의 날짜로 바꾸었고 frontend에서 데이터 요청 시 파일의 모든 데이터를 가져오게 바꿨습니다

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const mongodb = require('mongodb')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = 'mongodb+srv://Report:report@cluster0.2nwmd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const ID = 'Report'
+const PW = 'report'
+const MONGODB_URI = 'mongodb+srv://' + ID + ':' + PW + '@cluster0.2nwmd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const fs = require('fs');
 const csv = require('csvtojson')
 
@@ -23,7 +25,7 @@ mongoose.connection.on('connected', () => {
 //file read
 const dir = __dirname + '\\files'
 const files = fs.readdirSync(dir)
-console.log(files.length)
+
 //schema 자료형 구조
 const Report = new mongoose.Schema({
     IP: String,
@@ -53,6 +55,8 @@ const Report = new mongoose.Schema({
     'Other References': String
 });
 
+const DataSend = []
+
 //dir files 폴더 내의 파일들을 읽어서 db의 schema 생성
 //file의 이름을 가진 schema를 Report 모델에 생성
 //file 이름 기준 중복 방지
@@ -60,16 +64,49 @@ const Report = new mongoose.Schema({
 for (const file of files) {
     const fp = dir + '\\' + file
     const name = file.split('.')
-
-    const reportModel = mongoose.model(name[0], Report);
+    
     csv()
         .fromFile(fp)
         .then((jsonObj) => {
+            const time = jsonObj[0].Timestamp
+            const reportModel = mongoose.model(time, Report);
             reportModel.find({})
                 .then((data) => {
+                    for (item in jsonObj) {
+                        DataSend.push({
+                            IP: jsonObj[item].IP,
+                            Hostname: jsonObj[item].Hostname,
+                            Port: jsonObj[item].Port,
+                            Port_Protocol: jsonObj[item]['Port Protocol'],
+                            CVSS: jsonObj[item].CVSS,
+                            Severity: jsonObj[item].Severity,
+                            Solution_Type: jsonObj[item]['Solution Type'],
+                            NVT_Name: jsonObj[item]['NVT Name'],
+                            Summary: jsonObj[item].Summary,
+                            Specific_Result: jsonObj[item]['Specific Result'],
+                            NVT_OID: jsonObj[item]['NVT OID'],
+                            CVEs: jsonObj[item].CVEs,
+                            Task_ID: jsonObj[item]['Task ID'],
+                            Task_Name: jsonObj[item]['Task Name'],
+                            Timestamp: jsonObj[item].Timestamp,
+                            Result_ID: jsonObj[item]['Result ID'],
+                            Impact: jsonObj[item].Impact,
+                            Solution: jsonObj[item].Solution,
+                            Affected_Software_OS: jsonObj[item]['Affected Software/OS'],
+                            Vulnerability_Insight: jsonObj[item]['Vulnerability Insight'],
+                            Vulnerability_Detection_Method: jsonObj[item]['Vulnerability Detection Method'],
+                            Product_Detection_Result: jsonObj[item]['Product Detection Result'],
+                            BIDs: jsonObj[item].BIDs,
+                            CERTs: jsonObj[item].CERTs,
+                            Other_References: jsonObj[item]['Other References'],
+                            ITEM: files.length
+                        });
+                    }
+
                     if (data == undefined || data.length == 0) {
                         console.log('inserting data')
                         for (item in jsonObj) {
+                            console.log(jsonObj[item])
                             new reportModel(jsonObj[item])
                                 .save()
                                 .catch((err) => {
@@ -89,18 +126,7 @@ for (const file of files) {
 for (const file of files) {
     app.get('/api', (req, res) => {
         res.setHeader('Access-Control-Allow-origin', '*');
-        const fp = dir + '\\' + file
-        const name = file.split('.')
-        const reportModel = mongoose.model(name[0], Report);
-
-        reportModel.find({})
-            .then((data) => {
-                res.json(data)
-            })
-
-            .catch((error) => {
-                console.log('error');
-            })
+        res.json(DataSend)
     })
 }
 
