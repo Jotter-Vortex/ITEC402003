46dcaaaa3a911eb409936b8e4acae2d82055bdf2
박건우 <33975284+qkrrjsdn@users.noreply.github.com>
2022-05-17 12:04:32 +0900

    server port 5000
    서버 포트 5천번으로 변경

--- monitoring_web/server.js
+++ monitoring_web/server.js
@@ -5,36 +5,27 @@ const csvtojson = require('csvtojson');
 const mongodb = require('mongodb')
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const MONGODB_URI = 'mongodb+srv://Report:report@cluster0.2nwmd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const fs = require('fs');
-const { PermScanWifiOutlined } = require('@mui/icons-material');
-
+const csv = require('csvtojson')
 
 //mongodb connection
 mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 mongoose.connection.on('connected', () => {
-  console.log('Mongoose is connected!!!!');
+    console.log('Mongoose is connected!!!!');
 });
 
 //file read
 const dir = __dirname + '\\files'
 const files = fs.readdirSync(dir)
-const Schema = mongoose.Schema;
-
-//dir files 폴더 내의 파일들을 읽어서 db의 schema 생성
-//file의 이름을 가진 schema를 Report 모델에 생성
-
-for (const file of files) {
-  const fp = dir + '\\' + file
-  const name = file.split('.')
-
-  //schema 자료형 구조
-  const Report = mongoose.model(name[0], Schema({
+console.log(files.length)
+//schema 자료형 구조
+const Report = new mongoose.Schema({
     IP: String,
     Hostname: String,
     Port: Number,
@@ -60,51 +51,94 @@ for (const file of files) {
     BIDs: String,
     CERTs: String,
     'Other References': String
-  }));
+});
 
-  const csv = require('csvtojson')
+const DataSend = [[
+    {
+        IP: String,
+        Hostname: String,
+        Port: Number,
+        Port_Protocol: String,
+        CVSS: String,
+        Severity: String,
+        Solution_Type: String,
+        NVT_Name: String,
+        Summary: String,
+        Specific_Result: String,
+        NVT_OID: String,
+        CVEs: String,
+        Task_ID: String,
+        Task_Name: String,
+        Timestamp: String,
+        Result_ID: String,
+        Impact: String,
+        Solution: String,
+        Affected_Software_OS: String,
+        Vulnerability_Insight: String,
+        Vulnerability_Detection_Method: String,
+        Product_Detection_Result: String,
+        BIDs: String,
+        CERTs: String,
+        Other_References: String
+    }
+]]
 
-  const Pfile = 0
 
-  //검사 시간 기준으로 데이터 중복 확인 및 삽입
-  csv()
-    .fromFile(fp)
-    .then((jsonObj) => {
-      for (item in jsonObj) {
+//dir files 폴더 내의 파일들을 읽어서 db의 schema 생성
+//file의 이름을 가진 schema를 Report 모델에 생성
+//file 이름 기준 중복 방지
 
-        Report.find({})
-          .then((data) => {
-            if (data == undefined || data.length == 0 || data[0].Timestamp != jsonObj[item].Timestamp) {
-              console.log('inserting data')
-              new Report(jsonObj[item])
-                .save()
-                .catch((err) => {
-                  console.log(err.message);
-                });
-            }
+for (const file of files) {
+    var i = 0
+    const fp = dir + '\\' + file
+    const name = file.split('.')
 
-            else {
-              console.log('already exisiting')
-            }
-          })  
-      }
+    const reportModel = mongoose.model(name[0], Report);
+    csv()
+        .fromFile(fp)
+        .then((jsonObj) => {
+            reportModel.find({})
+                .then((data) => {
+                    for (item in jsonObj) {
+
+                    }
+
+                    if (data == undefined || data.length == 0) {
+                        console.log('inserting data')
+                        console.log(item in jsonObj)
+                        for (item in jsonObj) {
+                            new reportModel(jsonObj[item])
+                                .save()
+                                .catch((err) => {
+                                    console.log(err.message);
+                                });
+                        }
+                    }
+
+                    else {
+                        console.log('already exisiting')
+                    }
+                })
+        })
+}
+
+//cors policy avoid
+for (const file of files) {
+    app.get('/api', (req, res) => {
+        res.setHeader('Access-Control-Allow-origin', '*');
+        const fp = dir + '\\' + file
+        const name = file.split('.')
+        const reportModel = mongoose.model(name[0], Report);
+
+        reportModel.find({})
+            .then((data) => {
+                res.json(data)
+            })
+
+            .catch((error) => {
+                console.log('error');
+            })
     })
-
-  //   //cors policy avoid
-  //   app.get('/api', (req, res) => {
-  //     res.setHeader('Access-Control-Allow-origin', '*');
-
-  //     newReport.find({})
-  //       .then((data) => {
-  //         console.log('Data: ', data);
-  //         res.json(data);
-  //       })
-
-  //       .catch((error) => {
-  //         console.log('error: ', daerrorta);
-  //       })
-  //   });
-
 }
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
